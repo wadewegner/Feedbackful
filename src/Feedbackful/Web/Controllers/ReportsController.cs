a264bd77004d213015f3c76cd51137c2143f5a0d
Wade Wegner <wade.wegner@gmail.com>
2014-11-12 22:16:32 -0800

    Added highcharts and sample

--- src/Feedbackful/Web/Controllers/ReportsController.cs
+++ src/Feedbackful/Web/Controllers/ReportsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
 
 namespace Web.Controllers
 {
@@ -11,7 +13,17 @@ namespace Web.Controllers
         // GET: Reports
         public ActionResult Index()
         {
-            return View();
+            var chart = new DotNet.Highcharts.Highcharts("chart")
+                .SetXAxis(new XAxis
+                {
+                    Categories = new[] { "Answer 1", "Answer 2", "Answer 3", "Answer 4" }
+                })
+                .SetSeries(new Series
+                {
+                    Data = new Data(new object[] { 25, 25, 25, 25 })
+                });
+
+            return View(chart);
         }
 
     }
